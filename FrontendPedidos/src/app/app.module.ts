b9d360f04474f2e3eb5dcd34bb374337deed6b0c
hugo valencia jr <huvalpajr10@gmail.com>
2022-11-25 17:52:09 -0500

    Reorganizacion repositorio

---
+++ FrontendPedidos/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './plantilla/barra-navegacion/barra-navegacion.component';
+import { InicioComponent } from './plantilla/inicio/inicio.component';
+import { ErrorComponent } from './plantilla/error/error.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PiePaginaComponent } from './plantilla/pie-pagina/pie-pagina.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BarraNavegacionComponent,
+    PiePaginaComponent,
+    InicioComponent,
+    ErrorComponent
+  ],
+  
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ FrontendPedidos/src/app/modulos/administracion/administracion-routing.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ValidadorSesionGuard } from 'src/app/guardianes/validador-sesion.guard';
+import { BuscarPersonaComponent } from './personas/buscar-persona/buscar-persona.component';
+import { CrearPersonaComponent } from './personas/crear-persona/crear-persona.component';
+import { EditarPersonaComponent } from './personas/editar-persona/editar-persona.component';
+import { EliminarPersonaComponent } from './personas/eliminar-persona/eliminar-persona.component';
+import { BuscarProductoComponent } from './productos/buscar-producto/buscar-producto.component';
+import { CrearProductoComponent } from './productos/crear-producto/crear-producto.component';
+import { EditarProductoComponent } from './productos/editar-producto/editar-producto.component';
+
+const routes: Routes = [
+  {
+    path: "crear-persona",
+    component: CrearPersonaComponent
+  },
+  {
+    path: "buscar-persona",
+    component: BuscarPersonaComponent,
+    canActivate: [ValidadorSesionGuard]
+  },
+  {
+    path: "editar-persona",
+    component: EditarPersonaComponent,
+    canActivate: [ValidadorSesionGuard]
+  },
+  {
+    path: "eliminar-persona",
+    component: EliminarPersonaComponent,
+    canActivate: [ValidadorSesionGuard]
+  },
+  {
+    path: "listar-productos",
+    component: BuscarProductoComponent,
+    canActivate: [ValidadorSesionGuard]
+  },
+  {
+    path: 'crear-producto',
+    component: CrearProductoComponent,
+    canActivate: [ValidadorSesionGuard]
+  },
+  {
+    path: 'editar-producto/:id',
+    component: EditarProductoComponent,
+    canActivate: [ValidadorSesionGuard]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdministracionRoutingModule { }
